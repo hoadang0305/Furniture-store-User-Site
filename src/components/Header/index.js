17bf3d4e9b2e394af8bc49cf7249cce8a6db8e53
nhinbm <mnhi0507@gmail.com>
2023-10-18 20:07:32 +0700

    navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  CustomLayoutHeader,
+  HeaderButtonIcons,
+  HeaderIcons,
+  HeaderLogo,
+  HeaderName,
+  HeaderNav,
+  HeaderNavItem,
+} from "./styles";
+import Home from "../../pages/Home";
+import About from "../../pages/About";
+import {
+  HeartOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <CustomLayoutHeader>
+      <HeaderLogo to="/">
+        <img src="/logo_web.png" alt="logo" />
+        <HeaderName>Furniro</HeaderName>
+      </HeaderLogo>
+
+      <HeaderNav>
+        <HeaderNavItem to="/" element={<Home />}>
+          Home
+        </HeaderNavItem>
+        <HeaderNavItem to="/about" element={<About />}>
+          About
+        </HeaderNavItem>
+      </HeaderNav>
+
+      <HeaderIcons>
+        <HeaderButtonIcons icon={<UserOutlined />} size="large" />
+        <HeaderButtonIcons icon={<SearchOutlined />} size="large" />
+        <HeaderButtonIcons icon={<HeartOutlined />} size="large" />
+        <HeaderButtonIcons icon={<ShoppingCartOutlined />} size="large" />
+      </HeaderIcons>
+    </CustomLayoutHeader>
+  );
 };
 
 export default Header;
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+/*  file constants routes */
+
+// constant main navigation
+export const MAIN_NAVIGATION = [
+  {
+    key: "home",
+    path: "/",
+    title: "",
+    Component: React.lazy(() => import("./pages/Home")),
+  },
+  {
+    key: "about",
+    path: "/about",
+    title: "",
+    Component: React.lazy(() => import("./pages/About")),
+  },
+];
